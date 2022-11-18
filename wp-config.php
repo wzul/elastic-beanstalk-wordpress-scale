1195615d6228a232d0e372c753a6c80450cf6345
mamadcloud <muhammadhsn.hsn@gmail.com>
2022-11-19 06:46:18 +0800

    feat: add connection SES and set max limit vars

--- wp-config.php
+++ wp-config.php
@@ -3,22 +3,26 @@
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
     $_SERVER['HTTPS']='on';
 
-define('DB_NAME',           $_SERVER['RDS_DB_NAME']);
-define('DB_USER',           $_SERVER['RDS_USERNAME']);
-define('DB_PASSWORD',       $_SERVER['RDS_PASSWORD']);
-define('DB_HOST',           $_SERVER['RDS_HOSTNAME'] . ':' . $_SERVER['RDS_PORT']);
-define('DB_CHARSET',        'utf8');
-define('DB_COLLATE',        '');
-define('AUTH_KEY',          $_SERVER['AUTH_KEY']);
-define('SECURE_AUTH_KEY',   $_SERVER['SECURE_AUTH_KEY']);
-define('LOGGED_IN_KEY',     $_SERVER['LOGGED_IN_KEY']);
-define('NONCE_KEY',         $_SERVER['NONCE_KEY']);
-define('AUTH_SALT',         $_SERVER['AUTH_SALT']);
-define('SECURE_AUTH_SALT',  $_SERVER['SECURE_AUTH_SALT']);
-define('LOGGED_IN_SALT',    $_SERVER['LOGGED_IN_SALT']);
-define('NONCE_SALT',        $_SERVER['NONCE_SALT']);
-define('WP_HOME',           $_SERVER['WP_HOME']);
-define('WP_SITEURL',        $_SERVER['WP_SITEURL']);
+define('DB_NAME',                       $_SERVER['RDS_DB_NAME']);
+define('DB_USER',                       $_SERVER['RDS_USERNAME']);
+define('DB_PASSWORD',                   $_SERVER['RDS_PASSWORD']);
+define('DB_HOST',                       $_SERVER['RDS_HOSTNAME'] . ':' . $_SERVER['RDS_PORT']);
+define('DB_CHARSET',                    'utf8');
+define('DB_COLLATE',                    '');
+define('AUTH_KEY',                      $_SERVER['AUTH_KEY']);
+define('SECURE_AUTH_KEY',               $_SERVER['SECURE_AUTH_KEY']);
+define('LOGGED_IN_KEY',                 $_SERVER['LOGGED_IN_KEY']);
+define('NONCE_KEY',                     $_SERVER['NONCE_KEY']);
+define('AUTH_SALT',                     $_SERVER['AUTH_SALT']);
+define('SECURE_AUTH_SALT',              $_SERVER['SECURE_AUTH_SALT']);
+define('LOGGED_IN_SALT',                $_SERVER['LOGGED_IN_SALT']);
+define('NONCE_SALT',                    $_SERVER['NONCE_SALT']);
+define('WP_HOME',                       $_SERVER['WP_HOME']);
+define('WP_SITEURL',                    $_SERVER['WP_SITEURL']);
+
+## EMAIL SES: INSTALL PLUGIN "OFFLOAD SES LITE"
+define('WPOSES_AWS_ACCESS_KEY_ID',      $_SERVER['WPOSES_AWS_ACCESS_KEY_ID']);
+define('WPOSES_AWS_SECRET_ACCESS_KEY',  $_SERVER['WPOSES_AWS_SECRET_ACCESS_KEY']);
 
 $table_prefix  = 'wp_';
 define('WP_DEBUG', false);
