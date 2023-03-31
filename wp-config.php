ffbd9bebbf030bd559223e515458ebd620abed1e
mamadcloud <muhammadhsn.hsn@gmail.com>
2023-03-31 09:31:51 +0800

    chore: add more to config

--- wp-config.php
+++ wp-config.php
@@ -29,6 +29,7 @@ define( 'AS3CF_SETTINGS', serialize( array(
 	'access-key-id' => $_SERVER['S3_ACCESS_KEY'],
 	'secret-access-key' => $_SERVER['S3_SECRET_ACCESS_KEY'],
 ) ) );
+define('DISABLE_WP_CRON',               true);
 
 $table_prefix  = 'wp_';
 define('WP_DEBUG', $_SERVER['WP_DEBUG'] == 'true');
@@ -36,18 +37,17 @@ define('WP_DEBUG_LOG', $_SERVER['WP_DEBUG_LOG'] == 'true');
 
 /*@DOCKETCACHE-RUNTIME-BEGIN*/
 if(!\function_exists('docketcache_runtime')){
- function docketcache_runtime(){
-  if(!(\PHP_VERSION_ID >= 70205)) {return;}
-  try{
-   $path="/var/app/current/wp-content/docket-cache-data";
-   $runtime=$path."/runtime.php";
-   if(is_file($runtime)){include_once $runtime;}
-  }catch(\Throwable $e){}
+  function docketcache_runtime(){
+   if(!(\PHP_VERSION_ID >= 70205)) {return;}
+   try{
+    $path="/var/app/current/wp-content/docket-cache-data";
+    $runtime=$path."/runtime.php";
+    if(is_file($runtime)){include_once $runtime;}
+   }catch(\Throwable $e){}
+  }
+  docketcache_runtime();
  }
- docketcache_runtime();
-}
-/*@DOCKETCACHE-RUNTIME-END*/
-
+ /*@DOCKETCACHE-RUNTIME-END*/
 
 if ( !defined('ABSPATH') )
         define('ABSPATH', dirname(__FILE__) . '/');
